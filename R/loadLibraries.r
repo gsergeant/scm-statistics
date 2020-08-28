5fc8d18d95e671334d0f64b5fe65b35ef41c1485
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2020-08-28 13:40:58 +0200

    Add some libraries

--- R/loadLibraries.r
+++ R/loadLibraries.r
@@ -4,10 +4,14 @@ library(spatstat)
 library(colorRamps)
 # prettier plots
 library(ggplot2)
+#for biplots (pca)
+library(ggfortify)
 #probabilistic index models
 library(pim)
 # moments, skewness, kurtosis
 library(moments)
+# best standardization to normal distribution
+library(bestNormalize)
 # multiple group comparisons
 library(multcomp)
 # statistics and better qq plot
@@ -18,4 +22,8 @@ library(Hmisc)
 library(UsingR)
 
 
-######### placeholder for unnecessary packages ###
+######### placeholder for unnecessary packages/packages we are not using but need to be installed ###
+library(MASS)
+library(rlang)
+library(vctrs)
+library(backports)
