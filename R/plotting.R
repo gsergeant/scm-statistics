48b8575d3636a60ed3372a83a552b2988ab19ede
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2020-09-04 16:11:04 +0200

    Add in double colon method for future package

--- R/plotting.R
+++ R/plotting.R
@@ -1,15 +1,15 @@
 
 
 plot_coordinates <- function(dataset, subset_column, subset_factor) {
-  b <- ggplot(data = dataset[which(dataset[[subset_column]] == subset_factor), ], aes(x = X, y = Y))
-  b + geom_point(shape = 20,
-                 aes(color = track_id),
+  b <- ggplot2::ggplot(data = dataset[which(dataset[[subset_column]] == subset_factor), ], ggplot2::aes(x = X, y = Y))
+  b + ggplot2::geom_point(shape = 20,
+                 ggplot2::aes(color = track_id),
                  show.legend = FALSE)
 }
 
 plot_turningangles <- function(dataset, subset_column, subset_factor){
-  ggplot(dataset[which(dataset[[subset_column]] == subset_factor), ], aes(x = ta_deg)) +
-    geom_histogram(binwidth = 8) +
-    scale_x_continuous(breaks = seq(0, 360, 60)) +
-    coord_polar()
+  ggplot2::ggplot(dataset[which(dataset[[subset_column]] == subset_factor), ], ggplot2::aes(x = ta_deg)) +
+    ggplot2::geom_histogram(binwidth = 8) +
+    ggplot2::scale_x_continuous(breaks = seq(0, 360, 60)) +
+    ggplot2::coord_polar()
 }
