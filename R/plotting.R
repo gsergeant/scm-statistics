9bac218f54fdd2dd1643d03038fc476c90c87a7f
Gwendolien Sergeant <gwendolien.sergeant@ugent.be>
2020-09-15 15:08:40 +0200

    Speed profile plot for step-centric speed over time

--- R/plotting.R
+++ R/plotting.R
@@ -13,3 +13,13 @@ plot_turningangles <- function(dataset, subset_column, subset_factor){
     ggplot2::scale_x_continuous(breaks = seq(0, 360, 60)) +
     ggplot2::coord_polar()
 }
+
+
+#test to plot step data features over time
+plot_speedProfile <- function(dataset, subset_column, subset_factor) {
+  b <- ggplot2::ggplot(data = dataset[which(dataset[[subset_column]] == subset_factor), ], ggplot2::aes(x = frame_id, y = speed))
+  b + ggplot2::geom_point(size = 3, shape = 20,
+                          ggplot2::aes(color = track_id),
+                          show.legend = FALSE)+
+    ggplot2::geom_line(col = 'gray')
+}
